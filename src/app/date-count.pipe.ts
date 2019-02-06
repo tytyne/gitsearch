513572d15bb5c90968af112569a497183088a2a4
tytyne <dusaflora2@gmail.com>
2019-02-06 10:29:41 +0200

    adding creation date of github

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,27 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'dateCount'
+  name: "dateCount"
 })
 export class DateCountPipe implements PipeTransform {
+  transform(value: any): number {
+    let today: Date = new Date(); //get current date and time
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    var dateDifference = Math.abs(value - todayWithNoTime); // returns value in milliseconds
+    const secondsInADay = 86400; //60 seconds*60 minutes in an hour *24 hours
 
-  transform(value: any, args?: any): any {
-    return null;
+    var dateDifferenceSeconds = dateDifference * 0.001; //converts to seconds
+
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
+    console.log(todayWithNoTime);
+    if (dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
-
 }
